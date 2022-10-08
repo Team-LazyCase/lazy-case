9f1827c980c5ebf3cba5980a042d4d9d7da3f92c
JonasFP <jfieldpatton@gmail.com>
2022-10-07 22:36:00 -0700

    Set up script.

---
+++ addOn.js
@@ -0,0 +1,25 @@
+function onOpen() {
+    DocumentApp.getUi()
+        .createMenu('LazyCase')
+        .addItem('Run MLA', 'runMLA')
+        .addItem('Run AP', 'runAP')
+        .addItem('Run Chicago', 'runChicago')
+        .addItem('Run APA', 'runAPA')
+        .addToUi();
+}
+
+function runMLA() {
+    // Jonas
+}
+
+function runAP() {
+    // Mason
+}
+
+function runChicago() {
+    // James
+}
+
+function runAPA() {
+    // Brandon
+}
