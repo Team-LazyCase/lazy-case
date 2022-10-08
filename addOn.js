7155cb99e2639ade6afbc6d47f4977d44b69c166
JonasFP <jfieldpatton@gmail.com>
2022-10-08 08:14:32 -0700

    Insert initial prototype code into main file - Jonas.

--- addOn.js
+++ addOn.js
@@ -8,8 +8,26 @@ function onOpen() {
         .addToUi();
 }
 
+// A copy-paste function that capitalizes the first letter of every word.
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
 function runMLA() {
-    // Jonas
+    // Get and store the document's content as a single string.
+    var document = DocumentApp.getActiveDocument();
+    var body = document.getBody();
+    var inputText = body.getText();
+
+    // Turn the first letter and any other letter after a space into a capital.
+    outputText = toTitleCase(inputText);
+
+    // Replace the document's existing text with the text inside this string.
+    textElement = body.editAsText();
+    textElement.deleteText(0, textElement.getText().length - 1);
+    textElement.appendText(outputText);
 }
 
 function runAP() {
