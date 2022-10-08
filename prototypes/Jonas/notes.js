7c57644f1c8828fac06d436800e81dbc18b11d13
JonasFP <jfieldpatton@gmail.com>
2022-10-08 12:00:44 -0700

    Update personal notes. - Jonas

--- prototypes/Jonas/notes.js
+++ prototypes/Jonas/notes.js
@@ -1,1 +1,12 @@
 /* This is where I keep my notes on JavaScript and Google's Services. */
+
+function onOpen() {
+    DocumentApp.getUi()
+        .createMenu('Jonas Prototype')
+        .addItem('Run', 'sandbox')
+        .addToUi();
+}
+
+function sandbox() {
+    
+}
