2d4dffc49d9f8b673385ac362577cb8c49ae8636
baldenosu <baldenj@oregonstate.edu>
2022-10-08 14:31:55 -0600

    Main Added functions for line spacing and horizontal alignment to Chicago

--- prototypes/James/LazyCaseChicagoProtoype.js
+++ prototypes/James/LazyCaseChicagoProtoype.js
@@ -25,6 +25,8 @@ function runChicago() {
     // -block quotes, blank line around, additional 1/2 inch indent, single spaced.
     setParagraphAlignment("LEFT")
     setParagraphSpacing(2);
+    setPageMargins(72, 72, 72, 72);
+    setParagraphIndentation(36);
     
     
 }
@@ -44,3 +46,30 @@ function setParagraphSpacing(spaces) {
       paragraphs[i].setLineSpacing(spaces);
     }
 }
+
+function setPageMargins(top, bottom, left, right) {
+    // Function that sets the page margins for the document. Specified in points.
+    let body = DocumentApp.getActiveDocument().getBody();
+    body.setMarginTop(top);
+    body.setMarginBottom(bottom);
+    body.setMarginLeft(left);
+    body.setMarginRight(right);
+}
+
+function setParagraphIndentation(space) {
+    // Function that sets the indentation for the first line of a paragraph. Specified in points
+   let paragraphs = DocumentApp.getActiveDocument().getBody().getParagraphs();
+    for (let i = 0;i < paragraphs.length;i++) {
+      paragraphs[i].setIndentFirstLine(space);
+    }
+}
+
+// function setPageNumbers() {
+//   DocumentApp.getActiveDocument().addHeader();
+//     let header = DocumentApp.getActiveDocument().getHeader();
+    
+// }
+
+// function createBlockQuote() {
+
+// }
